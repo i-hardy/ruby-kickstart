2463f02261ddcf739039e1b2ad0e4a9ac0185cce
Imogen Hardy <imogen.hardy@gmail.com>
2017-06-13 18:35:08 +0100

    Session 1 Challenges

--- session1/challenge/6_string.rb
+++ session1/challenge/6_string.rb
@@ -1,11 +1,23 @@
 # You'll get a string and a boolean.
 # When the boolean is true, return a new string containing all the odd characters.
 # When the boolean is false, return a new string containing all the even characters.
-# 
+#
 # If you have no idea where to begin, remember to check out the cheatsheets for string and logic/control
-# 
+#
 # odds_and_evens("abcdefg",true)    # => "bdf"
 # odds_and_evens("abcdefg",false)   # => "aceg"
 
 def odds_and_evens(string, return_odds)
+  string = string.split(//)
+  output = []
+    if return_odds == true
+      string.each_with_index do |letter, index|
+      output.push(letter) if (index % 2 != 0)
+      end
+    else
+      string.each_with_index do |letter, index|
+      output.push(letter) if (index % 2 == 0)
+      end
+    end
+  output.join
 end
--- session1/challenge/7_string.rb
+++ session1/challenge/7_string.rb
@@ -1,8 +1,21 @@
 # given a string, return the character after every letter "r"
-# 
+#
 # pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
 # pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+  pirates = ""
+  string = string.downcase
+  add_char = false
+  string.split(//).each do |rcheck|
+    if rcheck != "r"
+      pirates << rcheck if add_char
+      add_char = false
+    else
+      pirates << rcheck if add_char
+      add_char = true
+    end
+  end
+  pirates
 end
