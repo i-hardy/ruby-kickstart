428e3227d30ea8342adfc93d4317af390850a1c5
Imogen Hardy <imogen.hardy@gmail.com>
2017-06-14 17:05:11 +0100

    Session 2 Challenges 8 - 13

--- session2/challenge/11_classes.rb
+++ session2/challenge/11_classes.rb
@@ -12,9 +12,59 @@
 # Zero bottles of beer on the wall.
 #
 # Your program should not use ninety-nine output statements!
-# Design your program with a class named BeerSong whose initialize method 
+# Design your program with a class named BeerSong whose initialize method
 # receives a parameter indicating the number of bottles of beer initially on the wall.
 # If the parameter is less than zero, set the number of bottles to zero. Similarly,
 # if the parameter is greater than 99, set the number of beer bottles to 99
 # Then make a public method called print_song that outputs all stanzas from the number of bottles of beer down to zero.
 # Add any additional methods you find helpful.
+
+class BeerSong
+  def initialize(beers)
+    if beers < 0
+      @beers = 0
+    elsif beers > 99
+      @beers = 99
+    else
+      @beers = beers
+    end
+  end
+
+  def print_song
+    @beers.downto 1 do |x|
+      stanza x
+    end
+  end
+
+  def stanza(x)
+    if x == 0
+      String.new
+    else
+      puts "#{english x} #{bottles x} of beer on the wall,",
+           "#{english x} #{bottles x} of beer,",
+           "Take one down, pass it around,",
+           "#{english x - 1} #{bottles x - 1} of beer on the wall."
+    end
+  end
+
+  def bottles(x)
+    if x == 1
+      "bottle"
+    else
+      "bottles"
+    end
+  end
+
+  def english(x)
+    if 0 <= x && x <= 19
+      to_twenty = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
+      in_english = to_twenty[x]
+    elsif x % 10 == 0
+      tens = ["zero", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+      in_english = tens[x / 10]
+    else
+      in_english = "#{english x/10*10}-#{english x%10}"
+    end
+    in_english.capitalize
+  end
+end
